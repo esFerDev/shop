02d3677a55ab702458ee711b811c73d15cd8722f
esFerDev <58149189+esFerDev@users.noreply.github.com>
2023-12-25 17:24:22 +0100

    Update index.js

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,12 +1,4 @@
 import DefaultTheme from "vitepress/theme";
 import "./custom.css";
 
-
-import Locale from './Locale.vue'
-
-export default {
-  ...DefaultTheme,
-  // override the Layout with a wrapper component that
-  // injects the slots
-  Layout: Locale
-}
+export default DefaultTheme;
