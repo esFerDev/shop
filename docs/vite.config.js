0af61d149159309cdba7c22120d3ef9d32a961a7
esFerDev <58149189+esFerDev@users.noreply.github.com>
2023-12-25 16:38:34 +0100

    Update vite.config.js

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -4,8 +4,8 @@ import { defineConfig } from "vite";
 //default options
 var options = {
   previewLength: 62,
-  buttonLabel: "Buscar",
-  placeholder: "Buscar...",
+  buttonLabel: "Search",
+  placeholder: "Searh documentation...",
 };
 
 export default defineConfig({
